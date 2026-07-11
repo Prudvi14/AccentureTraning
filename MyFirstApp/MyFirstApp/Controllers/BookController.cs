5cb0c8aa960643a07d981d29b38143aa540059de
Prudvi14 <gadeshulaprudhvi2004@gmail.com>
2026-07-11 18:54:53 +0530

    Day 12 Forms and Validation in Accenture .Net Training

--- MyFirstApp/MyFirstApp/Controllers/BookController.cs
+++ MyFirstApp/MyFirstApp/Controllers/BookController.cs
@@ -3,39 +3,63 @@ using MyFirstApp.Models;
 
 namespace MyFirstApp.Controllers
 {
+    //public class BookController : Controller
+    //{
+    //    public IActionResult Details(int id)
+    //    {
+    //        List<Book> books = new List<Book>
+    //    {
+    //        new Book
+    //        {
+    //            Id = 101,
+    //            Title = "The Alchemist",
+    //            Author = "Paulo Coelho",
+    //            Price = 350,
+    //            Pages = 208
+    //        },
+    //        new Book
+    //        {
+    //            Id = 102,
+    //            Title = "Atomic Habits",
+    //            Author = "James Clear",
+    //            Price = 450,
+    //            Pages = 320
+    //        },
+    //        new Book
+    //        {
+    //            Id = 103,
+    //            Title = "1984",
+    //            Author = "George Orwell",
+    //            Price = 299,
+    //            Pages = 328
+    //        }
+    //    };
+    //        Book selectedBook = books.FirstOrDefault(b => b.Id == id);
+    //        return View(selectedBook); 
+    //    }
+    //}
     public class BookController : Controller
     {
-        public IActionResult Details(int id)
+        [HttpGet]
+        public IActionResult Create()
         {
-            List<Book> books = new List<Book>
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create(Book book)
+        //{
+        //    return Content("You entered: " + book.Title+", Author: "+book.Author+", price: "+book.Price+" and pages: "+book.Pages);
+        //}
+
+        [HttpPost]
+        public IActionResult Create(Book book)
         {
-            new Book
+            if (!ModelState.IsValid)
             {
-                Id = 101,
-                Title = "The Alchemist",
-                Author = "Paulo Coelho",
-                Price = 350,
-                Pages = 208
-            },
-            new Book
-            {
-                Id = 102,
-                Title = "Atomic Habits",
-                Author = "James Clear",
-                Price = 450,
-                Pages = 320
-            },
-            new Book
-            {
-                Id = 103,
-                Title = "1984",
-                Author = "George Orwell",
-                Price = 299,
-                Pages = 328
+                return View(book);
             }
-        };
-            Book selectedBook = books.FirstOrDefault(b => b.Id == id);
-            return View(selectedBook); 
+            return View("Success", book);
         }
     }
 }
--- MyFirstApp/MyFirstApp/Models/Book.cs
+++ MyFirstApp/MyFirstApp/Models/Book.cs
@@ -1,11 +1,31 @@
-﻿namespace MyFirstApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstApp.Models
 {
+    //public class Book
+    //{
+    //    public int Id { get; set; }
+    //    public string Title { get; set; }
+    //    public string Author { get; set; }
+    //    public decimal Price { get; set; }
+    //    public int Pages { get; set; }
+    //}
+
     public class Book
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
         public string Author { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Price must be between 1 and 100000")]
         public decimal Price { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Pages must be a positive number")]
         public int Pages { get; set; }
     }
 }
