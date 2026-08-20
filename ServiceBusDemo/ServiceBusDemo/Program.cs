6bbd4a89713af7dccea42f1cb8637847b771f433
Prudvi14 <gadeshulaprudhvi2004@gmail.com>
2026-08-20 21:52:31 +0530

    practicing C# and Curd operation in ASP.NET core

---
+++ ServiceBusDemo/ServiceBusDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // 1. Service Bus Topic - Connection
+        string connectionString = "";
+        string topicName = "firsttopic";
+
+        await using var client = new ServiceBusClient(connectionString);
+
+        // 2. Create a sender for the topic
+        ServiceBusSender sender = client.CreateSender(topicName);
+
+        // 3. Send the message
+        await sender.SendMessageAsync(new ServiceBusMessage("My First Msg to Topic"));
+
+        Console.WriteLine("Message sent successfully......");
+    }
+}
